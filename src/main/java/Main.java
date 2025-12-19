58d78259ec241ea8a47a1c62e40b347c399e9b70
MarchenaNathan <nmarchena95@gmail.com>
2025-12-19 11:52:52 +0100

    Class+main+interface+methode combattre faites

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,8 @@ public class Main {
         Pokemon feu = new PokemonFeu("Salamèche", 5,25);
         Pokemon eau = new PokemonEau("Carapuce",5,25);
         Pokemon electrik = new PokemonElectrik("Pikachu de Sacha",10000,5000000);
+        Pokemon feu2 = new PokemonFeu("Groudon",60,250);
+        Pokemon eau2 = new PokemonEau("Kyogre",65,250);
 
         List<Pokemon> l = new ArrayList<>();
         l.add(feu);
@@ -17,6 +19,14 @@ public class Main {
             p.afficherInfos();
         }
 
+        List<Pokemon> l2 = new ArrayList<>();
+        l2.add(feu2);
+        l2.add(eau2);
 
+        for (Pokemon p2 : l2){
+            if (!p2.equals(feu2)){
+            p2.combattre(feu2);
+            }
+        }
     }
 }
--- src/main/java/Pokemon.java
+++ src/main/java/Pokemon.java
@@ -12,4 +12,32 @@ public abstract class Pokemon implements Attaquant{
     public void afficherInfos(){
         System.out.println("Ce pokemon est : " +nom+ ". Il est niveau : " + niveau + " et à " + pv + " de points de vie.");
     }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getNiveau() {
+        return niveau;
+    }
+
+    public void setNiveau(int niveau) {
+        this.niveau = niveau;
+    }
+
+    public int getPv() {
+        return pv;
+    }
+
+    public void setPv(int pv) {
+        this.pv = pv;
+    }
+
+    public void combattre(Pokemon combatant){
+        System.out.println( nom + " Combat "  + combatant.getNom() );
+    }
 }
